9ad4a7e61448109be2f96e1814f982fcfc887796
arcastilloiii@gmail.com <arcastilloiii@gmail.com>
2024-11-04 01:13:56 -0500

    Gnome switch

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -58,11 +58,11 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
-  #services.displayManager.gdm.enable = true;
-  #services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
