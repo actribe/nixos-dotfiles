775fe01e9d87d293c5a1dd3e55e54814e2e98ab9
Army Castillo <arcastilloiii@gmail.com>
2024-11-14 22:28:03 -0500

    added fw13 files

---
+++ hosts/fw13/default.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ...}:
+{
+	home.username="armyc";
+	home.homeDirectory = "/home/armyc";
+
+	home.stateVersion = "24.05";
+
+	home.packages = [
+
+	];
+
+	home.file = {
+	};
+
+	home.sessionVariables = {
+
+	};
+
+	programs.home-manager.enable = true;
+
+	programs.waybar.enable = true;
+	programs.rofi.enable = true;
+
+	programs.tmux.enable = true;
+	programs.fzf = {
+		enable = true;
+		enableZshIntegration = true;
+	};
+	programs.bat.enable = true;
+	programs.starship = {
+		enable = true;
+		enableZshIntegration = true;
+	};
+}
