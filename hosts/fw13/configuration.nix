1264b6edf6497e2800b36da0b8515516e1a37004
army castillo <arcastilloiii@gmail.com>
2024-11-28 00:55:50 -0500

    Hibernation Changes

--- hosts/fw13/configuration.nix
+++ hosts/fw13/configuration.nix
@@ -17,7 +17,7 @@
   ];
   swapDevices = [ { device = "/swapfile"; size = 64*1024; } ];
   boot.kernelParams = [ "resume_offset=7266373" ];
-  boot.resumeDevice = "/dev/disk/by-uuid/4992d1a4-b11e-4f4f-9063-81678b14f346";
+  boot.resumeDevice = "/dev/dm-0";
   boot.initrd.systemd.enable = true;
 
   # Bootloader.
