1f8ca181862704d46ce2bb6614a057207b7360ae
army castillo <arcastilloiii@gmail.com>
2024-12-07 16:54:59 -0500

    hosts/fw13 configuration.nix
    configuration.nix
    -> Added garbage collection

--- hosts/fw13/configuration.nix
+++ hosts/fw13/configuration.nix
@@ -54,6 +54,13 @@
     "flakes"
   ];
 
+  #Set up Garbage Collector
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
