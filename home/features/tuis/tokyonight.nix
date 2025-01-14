4df974e22803bddfc18a849695d3d2cf47626001
army castillo <arcastilloiii@gmail.com>
2025-01-13 21:29:41 -0500

    Added sha256

--- home/features/tuis/tokyonight.nix
+++ home/features/tuis/tokyonight.nix
@@ -4,5 +4,6 @@
       owner = "folke";
       repo = "tokyonight.nvim";
       rev = "775f82f08a3d1fb55a37fc6d3a4ab10cd7ed8a10";
+      sha256 = "1F8qJIt0YYCP4X/P54Z8By8f9/pp0YvAE6nISuDHhCs="
   };
 }
