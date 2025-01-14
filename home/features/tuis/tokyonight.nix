f5823543f5a7e2638f35aa9ecee278334da1fa30
army castillo <arcastilloiii@gmail.com>
2025-01-13 21:27:37 -0500

    attempting to add using homefile symlinks

--- home/features/tuis/tokyonight.nix
+++ home/features/tuis/tokyonight.nix
@@ -1,11 +1,8 @@
 { fetchFromGitHub, lib, pkgs, ... }:
 {
-  tokyonight = pkgs.stdenv.mkDerivation {
-  name = "tokyonight.nvim";
-  src = pkgs.fetchFromGitHub {
-    owner = "folke";
-    repo = "tokyonight.nvim";
-    rev = "775f82f08a3d1fb55a37fc6d3a4ab10cd7ed8a10";
-  };
+  home.file.".local/share/nvim/lazy".source = pkgs.fetchFromGitHub {
+      owner = "folke";
+      repo = "tokyonight.nvim";
+      rev = "775f82f08a3d1fb55a37fc6d3a4ab10cd7ed8a10";
   };
 }
