2f4be64ef08e78a4fe0b1697666654881dd2da02
army castillo <arcastilloiii@gmail.com>
2025-01-13 21:18:48 -0500

    Attempting to make stdenv for this

--- home/features/tuis/tokyonight.nix
+++ home/features/tuis/tokyonight.nix
@@ -1,9 +1,11 @@
 { fetchFromGitHub, lib, pkgs, ... }:
 {
-  source = pkgs.fetchFromGitHub {
+  tokyonight = pkgs.stdenv.mkDerivation {
+  name = "tokyonight.nvim";
+  src = pkgs.fetchFromGitHub {
     owner = "folke";
     repo = "tokyonight.nvim";
     rev = "775f82f08a3d1fb55a37fc6d3a4ab10cd7ed8a10";
   };
-
+  };
 }
