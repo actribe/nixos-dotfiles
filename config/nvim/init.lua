76e05e9378d780bbea77f50474bf53c2806bbcac
army castillo <arcastilloiii@gmail.com>
2025-01-15 00:41:35 -0500

    Added configuration specifications

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -26,6 +26,9 @@ require("lazy").setup({
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' }
     },
+    {
+      "neovim/nvim-lspconfig",
+    },
     { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
     {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
     {
